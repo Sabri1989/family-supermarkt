59791b2f881045fa04c2dc83e36e313086bbd319
Sabri1989 <mnawwarsabri@gmail.com>
2026-06-29 02:57:18 +0200

    clear black border

--- static/js/main.js
+++ static/js/main.js
@@ -709,7 +709,7 @@ document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
         if (navbarCollapse && navbarCollapse.classList.contains('show')) {
             const toggler = document.querySelector('.navbar-toggler');
             if (toggler) toggler.click();
-        }
+        } 
     });
 });p
 });
